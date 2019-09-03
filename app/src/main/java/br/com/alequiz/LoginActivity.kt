21dd1e675ae442c7d10c281621839f552767878c
guialves622@gmail.com <guialves622@gmail.com>
2019-09-02 21:58:39 -0300

    Commit inicial - Alequiz

---
+++ app/src/main/java/br/com/alequiz/LoginActivity.kt
@@ -0,0 +1,107 @@
+package br.com.alequiz
+
+import android.os.AsyncTask
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import kotlinx.android.synthetic.main.activity_login.*
+
+/**
+ * A login screen that offers login via email/password.
+ */
+class LoginActivity {
+
+    private var mGoogleSignInClient: GoogleSignInClient? = null
+    // Configure sign-in to request the user's ID, email address, and basic
+    // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+    var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestEmail()
+        .build()
+    private var mStatusTextView: TextView? = null
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+
+    private fun isEmailValid(email: String): Boolean {
+        //TODO: Replace this with your own logic
+        return email.contains("@")
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        //TODO: Replace this with your own logic
+        return password.length > 4
+    }
+
+    object ProfileQuery {
+        val PROJECTION = arrayOf(
+            ContactsContract.CommonDataKinds.Email.ADDRESS,
+            ContactsContract.CommonDataKinds.Email.IS_PRIMARY)
+        val ADDRESS = 0
+        val IS_PRIMARY = 1
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    inner class UserLoginTask internal constructor(private val mEmail: String, private val mPassword: String) : AsyncTask<Void, Void, Boolean>() {
+
+        override fun doInBackground(vararg params: Void): Boolean? {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000)
+            } catch (e: InterruptedException) {
+                return false
+            }
+
+            return DUMMY_CREDENTIALS
+                .map { it.split(":") }
+                .firstOrNull { it[0] == mEmail }
+                ?.let {
+                    // Account exists, return true if the password matches.
+                    it[1] == mPassword
+                }
+                ?: true
+        }
+
+//        override fun onPostExecute(success: Boolean?) {
+//            mAuthTask = null
+//            showProgress(false)
+//
+//            if (success!!) {
+//                finish()
+//            } else {
+//                password.error = getString(R.string.error_incorrect_password)
+//                password.requestFocus()
+//            }
+//        }
+
+//        override fun onCancelled() {
+//            mAuthTask = null
+//            showProgress(false)
+//        }
+
+
+    }
+    companion object {
+        /**
+         * Id to identity READ_CONTACTS permission request.
+         */
+        private val REQUEST_READ_CONTACTS = 0
+
+        /**
+         * A dummy authentication store containing known user names and passwords.
+         * TODO: remove after connecting to a real authentication system.
+         */
+        private val DUMMY_CREDENTIALS = arrayOf("foo@example.com:hello", "bar@example.com:world")
+    }
+}
